d9c7ba2dfefe364b923559a8fcde202cf151d4aa
Ruslan <ruspet55@ngs.ru>
2026-03-20 12:40:42 +0700

    Commit of VK files

--- VK_creating_embed_file.sh
+++ VK_creating_embed_file.sh
@@ -1,102 +1,106 @@
 #!/bin/bash
 
-# 1. Из HTML шаблона файла встраивания извлекаем window.RT_WIDGET_CONFIG                                    -> rt_widget_config
-# 2. Минифицируем rt_widget_config                                                                          -> rt_widget_config_minified
-# 3. Загружаем JSON для встраивания                                                                         -> json_response
-# 4. Минифицируем json_response                                                                             -> json_response_minified
-# 5. Убираем внешние фигурные скобки у json_response_minified                                               -> json_response_list_pairs
-# 6. В rt_widget_config_minified заменяем "placeholder_json_response": "value" на json_response_list_pairs  -> rt_widget_config_change1
-# 7. Преобразуем rt_widget_config_change1 в удобочитаемый вид rt_widget_config_beauty                       -> rt_widget_config_pretty
-# 8. В rt_widget_config_pretty заменяем "function(){}" на function () {}
-# 9. Копируем файл HTML шаблона в целевой файл
-#
-#
-#
+# 1.    Подготавливаем epc_widget_list_pairs
+# 1.1.  Загружаем JSON для встраивания из $PATH_JSON                                                        -> epc_widget_json
+# 1.2.  Значение "productOfferCfg" - замена с null на []                                                    -> epc_widget_json
+# 1.3.  Минифицируем epc_widget_json                                                                        -> epc_widget_minified
+# 1.4.  Убираем внешние фигурные скобки у json_response_minified                                            -> epc_widget_list_pairs
+# 2.    Подготовка rt_widget_config_pretty
+# 2.1.  Минифицируем RT_WIDGET_CONFIG                                                                       -> rt_widget_config_minified
+# 2.2.  Замена <"placeholder_json_response": "value"> на epc_widget_list_pairs в rt_widget_config_minified  -> rt_widget_config_minified
+# 2.3.  Преобразуем rt_widget_config_minified в удобочитаемый вид                                           -> rt_widget_config_beauty
+# 3.    Создание файла встройки
+# 3.1.  В переменной TEMPLATE меняем текст rt_widget_config_beauty на переменную rt_widget_config_beauty    -> vk_result
+# 3.2.  Создание файла PATH_RESULT с содержимым vk_result
+
+export LC_ALL=ru_RU.UTF-8
+export LANG=ru_RU.UTF-8
 
 # Объявление констант
-PATH_JSON="/home/ruslan/Documents/RTK_IT/productOfferCfg_1.json"
-PATH_TEMPLATE="/home/ruslan/Documents/RTK_IT/Template_html_1.html"
-PATH_HTML="/home/ruslan/Documents/RTK_IT/VK_html.html"
+PATH_JSON="/home/ruslan/Documents/RTK_IT/VK_productOffer.json"
+PATH_TEMPLATE="/home/ruslan/Documents/RTK_IT/VK_template.html"
+PATH_RESULT="/home/ruslan/Documents/RTK_IT/VK_result.html"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение PATH_JSON: $PATH_JSON"
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение PATH_TEMPLATE: $PATH_TEMPLATE"
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение PATH_HTML: $PATH_HTML"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение PATH_RESULT: $PATH_RESULT"
+
+TEMPLATE='<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=11">
+    <meta name="google" content="notranslate">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+</head>
+
+<body>
+    <div id="epc-widget"></div>
+    <script>window.RT_WIDGET_CONFIG = rt_widget_config_beauty</script>
+    <script src="https://epc-api.rt.ru/apiman-gateway/epc-front-wc/epc-offers-widget/1.8.4/static/js/app.js"></script>
+</body>
+
+</html>'
+
+RT_WIDGET_CONFIG='{
+    "epc-widget": {
+        "apiKey": "0106e80d-ae53-48e3-a12a-a1002896a7fb",
+        "placeholder_json_response": "value",
+        "contactInfo": {
+            "phoneNumber": "9139139139",
+            "email": ""
+        },
+        "differencePOC": false,
+        "outputData": {
+            "status": true,
+            "onChangePrice": "function(){}"
+        },
+        "_buildDate": "21.11.2024"
+    }
+}'
+
 
 echo ""
-echo "1. Извлекаем текущую конфигурацию rt_widget_config из HTML:"
-# Извлекаем текущую конфигурацию из HTML    <script>window.RT_WIDGET_CONFIG = {}</script>
-# Поиск с учетом вложенности (более надежный) Этот вариант использует рекурсивный regex и правильно обработает вложенные объекты
-rt_widget_config=$(perl -0777 -ne 'while (/window\.RT_WIDGET_CONFIG = ({((?>[^{}]+|(?1))*)})/g) { print $1 }' $PATH_TEMPLATE)
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение rt_widget_config: $rt_widget_config"
+echo "1.1.  Загружаем JSON для встраивания из $PATH_JSON:"
+epc_widget_json=$(cat "$PATH_JSON")
 
 echo ""
-echo "2. Минифицируем rt_widget_config:"
-rt_widget_config_minified=$(echo "$rt_widget_config" | jq -c .)
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение rt_widget_config_minified: $rt_widget_config_minified"
+echo "1.2.  Значение "productOfferCfg" - замена с null на []:"
+# product_offer_cfg=$(echo "$epc_widget_json" | jq -r '.data.productOfferCfg')
+product_offer_cfg=$(echo "$epc_widget_json" | jq -r '.productOfferCfg')
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение product_offer_cfg: $product_offer_cfg"
+# Обновляем исходный JSON с измененным productOfferCfg
+modified_product_offer_cfg='[]'
+epc_widget_json=$(echo "$epc_widget_json" | jq --argjson new "$modified_product_offer_cfg" '.productOfferCfg = $new')
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение epc_widget_json: $epc_widget_json"
 
 echo ""
-echo "3. Загружаем JSON для встраивания:"
-json_response=$(cat "$PATH_JSON")
+echo "1.3.  Минифицируем epc_widget_json:"
+epc_widget_minified=$(echo "$epc_widget_json" | jq -c .)
 
 echo ""
-echo "4. Минифицируем json_response:"
-json_response_minified=$(echo "$json_response" | jq -c .)
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение json_response_minified: $json_response_minified"
+echo "1.4.  Убираем внешние фигурные скобки у json_response_minified:"
+epc_widget_list_pairs="${epc_widget_minified:1:-1}"
 
 echo ""
-echo "5. Убираем внешние фигурные скобки у json_response_minified:"
-json_response_list_pairs="${json_response_minified:1:-1}"
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение json_response_list_pairs: $json_response_list_pairs"
+echo "2.1.  Минифицируем RT_WIDGET_CONFIG:"
+rt_widget_config_minified=$(echo "$RT_WIDGET_CONFIG" | jq -c .)
 
 echo ""
-echo "6. В rt_widget_config_minified заменяем "placeholder_json_response": "value" на json_response_list_pairs"
-rt_widget_config_change1="${rt_widget_config_minified//\"placeholder_json_response\":\"value\"/$json_response_list_pairs}"
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение rt_widget_config_change1: $rt_widget_config_change1"
+echo "2.2.  Замена <"placeholder_json_response": "value"> на epc_widget_list_pairs в rt_widget_config_minified:"
+rt_widget_config_minified="${rt_widget_config_minified//\"placeholder_json_response\":\"value\"/$epc_widget_list_pairs}"
 
 echo ""
-echo "7. Преобразуем rt_widget_config_change1 в удобочитаемый вид rt_widget_config_pretty"
-rt_widget_config_pretty=$(echo "$rt_widget_config_change1" | jq '.')
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение rt_widget_config_pretty: $rt_widget_config_pretty"
+echo "2.3.  Преобразуем rt_widget_config_minified в удобочитаемый вид:"
+rt_widget_config_beauty=$(echo "$rt_widget_config_minified" | jq '.')
+# echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение rt_widget_config_beauty: $rt_widget_config_beauty"
 
 echo ""
-echo "8. В rt_widget_config_pretty заменяем \"function(){}\" на function () {}"
-rt_widget_config=${rt_widget_config_pretty//"\"function(){}\""/function () \{ \}}
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение rt_widget_config: $rt_widget_config"
+echo "3.1.  В переменной TEMPLATE меняем <rt_widget_config_beauty> на переменную rt_widget_config_beauty:"
+vk_result="${TEMPLATE//rt_widget_config_beauty/$rt_widget_config_beauty}"
+# echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение rt_widget_config_beauty: $VK_result"
 
 echo ""
-echo "9. Копируем файл HTML шаблона $PATH_TEMPLATE в целевой файл $PATH_HTML"
-cp "$PATH_TEMPLATE" "$PATH_HTML"
-
-
-# echo "Экранируем значение для вставки в JSON"
-# escaped_config=$(echo "$rt_widget_config" | jq -R -s '.')
-# echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение escaped_config: $escaped_config"
-
-echo ""
-echo "11. В целевом файле $PATH_HTML меняем значение window.RT_WIDGET_CONFIG на rt_widget_config"
-# perl -i -0777 -pe 's/(window\.RT_WIDGET_CONFIG = )({((?>[^{}]+|(?1))*)})/$1$ENV{rt_widget_config}/g' "$PATH_HTML"
-# perl -i -0777 -pe "s/(window\.RT_WIDGET_CONFIG = )({((?>[^{}]+|(?1))*)})/\$1$rt_widget_config/g" "$PATH_HTML"
-# perl -i -0777 -pe 's/(window\.RT_WIDGET_CONFIG = )({((?>[^{}]+|(?1))*)})/$1$ARGV[0]/g' "$rt_widget_config" "$PATH_HTML"
-perl -i -0777 -pe 's/(window\.RT_WIDGET_CONFIG = )({((?>[^{}]+|(?1))*)})/$1 . quotemeta($ENV{rt_widget_config})/ge' \
-  -e "BEGIN { \$ENV{rt_widget_config} = '$rt_widget_config' }" "$PATH_HTML"
-
-# Заменяем placeholder
-# json_response=$(echo "$json_response" | jq --arg new "$escaped_config" '.widget_config = $new')
-
-echo ""
-# echo "$(date '+%Y-%m-%d %H:%M:%S') - Значение rt_widget_config: $rt_widget_config"
-echo ""
-
-
-# Замена $json_response на содержимое JSON в файле
-# Используем | как разделитель в sed, чтобы избежать проблем с символами / в JSON
-# sed -i "s|\\\$json_response|$json_response|g" "$PATH_HTML"
-
-# echo "Скрипт успешно выполнен. JSON вставлен в файл: $PATH_HTML"
-
-
-
-# не работают или работают плохо:
-# current_config=$(grep -o 'window\.RT_WIDGET_CONFIG = {[^}]*}' /home/ruslan/Documents/RTK_IT/Template_html_1.html)
-# current_config=$(perl -0777 -ne 'print "$&\n" if /window\.RT_WIDGET_CONFIG = {.*?}/s' /home/ruslan/Documents/RTK_IT/Template_html_1.html)
-# current_config=$(sed -n '/window\.RT_WIDGET_CONFIG = {/,/^}$/p' $PATH_TEMPLATE)
+echo "3.2.  Создание файла PATH_RESULT с содержимым vk_result:"
+echo "$vk_result" > $PATH_RESULT
